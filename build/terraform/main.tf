601945818e849c86be57bca594310d59aa1d6503
Samuel Stidham <dqfan2012@gmail.com>
2026-01-29 00:32:02 -0500

    Added missing ingress rule.

--- build/terraform/main.tf
+++ build/terraform/main.tf
@@ -54,6 +54,14 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Flask App"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
